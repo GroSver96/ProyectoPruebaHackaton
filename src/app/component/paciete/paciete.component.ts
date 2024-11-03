70b203dfad4be616bb0aecda0539b9adc5404962
JCrisRoquel <joelroque13677@gmail.com>
2024-11-03 18:59:00 -0400

    rama de joel

---
+++ src/app/component/paciete/paciete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paciete',
+  templateUrl: './paciete.component.html',
+  styleUrl: './paciete.component.css'
+})
+export class PacieteComponent {
+
+}
