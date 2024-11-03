666c251614861e9f0431ae46f17eb5c074d8ef50
JCrisRoquel <joelroque13677@gmail.com>
2024-11-03 19:55:45 -0400

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PacieteComponent } from './component/paciete/paciete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PacieteComponent
   ],
   imports: [
     BrowserModule,
