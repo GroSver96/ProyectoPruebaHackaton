35fc695f5fd1f458dc6913b5fbba297b4b41bbde
cortes150 <chicholas150@gmail.com>
2024-11-03 17:12:34 -0400

    Creando componente AmbulanceTracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmbulanceTrackingComponent } from './components/ambulance-tracking/ambulance-tracking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AmbulanceTrackingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/ambulance-tracking/ambulance-tracking.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ambulance-tracking',
+  templateUrl: './ambulance-tracking.component.html',
+  styleUrl: './ambulance-tracking.component.css'
+})
+export class AmbulanceTrackingComponent {
+
+}
