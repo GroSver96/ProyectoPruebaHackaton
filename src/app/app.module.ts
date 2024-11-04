c8478b5851d20b11566d5bf7643a80d1b1ff0805
Grover Hernani <hernanigrover@gmail.com>
2024-11-03 21:23:30 -0400

    Commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginGroverComponent } from './components/login-grover/login-grover.component';
 import { AmbulanceTrackingComponent } from './components/ambulance-tracking/ambulance-tracking.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginGroverComponent,
-    AmbulanceTrackingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-=======
 
 @NgModule({
-  declarations: [AppComponent, AmbulanceTrackingComponent],
+  declarations: [AppComponent, AmbulanceTrackingComponent, LoginGroverComponent],
   imports: [BrowserModule, AppRoutingModule],
->>>>>>> 4b163cf71457fc929ea6524957ce8774c5f62f06
   providers: [],
   bootstrap: [AppComponent],
 })
