c064303a0a8fb8fb65c9a81fb2cc0627117a8390
JCrisRoquel <joelroque13677@gmail.com>
2024-11-03 20:31:20 -0400

    actual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PacientRegistrationComponent } from './components/pacient-registration/pacient-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PacientRegistrationComponent
   ],
   imports: [
     BrowserModule,
