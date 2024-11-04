55064e541f46ff6c9ec8670f5c920d35ea4b7c64
JCrisRoquel <joelroque13677@gmail.com>
2024-11-03 21:21:33 -0400

    cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PacientRegistrationComponent } from './components/pacient-registration/pacient-registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PacientRegistrationComponent
+    PacientRegistrationComponent,
+    LoginComponent,
+    RegistrarComponent,
   ],
   imports: [
     BrowserModule,
