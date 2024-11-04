c937754108c7ecfe8c46e6a5f8bfea37c274c52e
JCrisRoquel <joelroque13677@gmail.com>
2024-11-03 22:12:49 -0400

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+import { PacientRegistrationComponent } from './components/pacient-registration/pacient-registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
 import { LoginGroverComponent } from './components/login-grover/login-grover.component';
 import { AmbulanceTrackingComponent } from './components/ambulance-tracking/ambulance-tracking.component';
 import { PatientTransportingNotificationComponent } from './components/patient-transporting-notification/patient-transporting-notification.component';
 
 @NgModule({
-  declarations: [AppComponent, AmbulanceTrackingComponent, LoginGroverComponent, PatientTransportingNotificationComponent],
-  imports: [BrowserModule, AppRoutingModule],
-=======
-import { PacientRegistrationComponent } from './components/pacient-registration/pacient-registration.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistrarComponent } from './components/registrar/registrar.component';
-
-
-@NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [AppComponent, AmbulanceTrackingComponent, LoginGroverComponent, PatientTransportingNotificationComponent,
     PacientRegistrationComponent,
     LoginComponent,
     RegistrarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
->>>>>>> joel_roque
+  imports: [BrowserModule, AppRoutingModule],
+
+
   providers: [],
   bootstrap: [AppComponent],
 })
